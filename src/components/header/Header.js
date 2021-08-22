8fb485155bba7edff4f8416e6a0f5c7414c37d87
Kory Koponen <koponenk@gmail.com>
2021-08-22 13:39:43 -0500

    responsive styling touchups

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 
 import logo from "../../assets/Logo/Logo-brainflix.svg"
-import searchIcon from "../../assets/Icons/Icon-search.svg"
+import uploadIcon from "../../assets/Icons/Icon-upload.svg"
 import React from 'react';
 import "../header/header.scss";
 
@@ -10,11 +10,11 @@ import "../header/header.scss";
 const Header = () => {
     return (
         <div className="navbar">
-            <img className="navbar__img" src={logo}/>
+            <img className="navbar__img" src={logo} alt="brainflix logo"/>
             <form className="navbar__form">
             <input className="navbar__search" type="text" placeholder="           Search"></input>
             <div className="navbar__box">
-            <button className="navbar__box--button">+ UPLOAD</button>
+            <button className="navbar__box--button"><img className="navbar__box--icon" src={uploadIcon} alt="upload icon"/>     UPLOAD</button>
             <div className="navbar__box--avatar"></div>
             </div>
             </form>
