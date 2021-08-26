ada6a56a228a2090eb8895d6c64b23d732db7e08
Kory Koponen <koponenk@gmail.com>
2021-08-26 18:50:54 -0500

    started styling videouploads page

--- src/pages/VideoUploadsPage/VideoUploadsPage.js
+++ src/pages/VideoUploadsPage/VideoUploadsPage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import videoThumb from '../../assets/Images/Upload-video-preview.jpg';
+import '../VideoUploadsPage/videoUploadsPage.scss';
 
 const VideoUploadsPage = () => {
     return (
-        <div>
+        <div className="video-uploads">
             <Header />
-            <h1>Upload Video</h1>
-            <p>VIDEO THUMBNAIL</p>
-            <img src={videoThumb} alt="bike handlebars"/>
+            <h1 className="video-uploads__title">Upload Video</h1>
+            <p className="video-uploads__subtitle">VIDEO THUMBNAIL</p>
+            <img className="video-uploads__img" src={videoThumb} alt="bike handlebars"/>
+            <form className="video-uploads__form">
+                <label className="video-uploads__form--label">TITLE YOUR VIDEO</label>
+                <input className="video-uploads__form--input" type="text" placeholder="Add a title to your video"></input>
+                <label className="video-uploads__form--label">ADD A VIDEO DESCRIPTION</label>
+                <textarea className="video-uploads__form--textarea" type="text" placeholder="Add a description of your video"></textarea>
+                <button className="video-uploads__form--button">PUBLISH</button>
+                <button className="video-uploads__form--cancel-button">CANCEL</button>
+            </form>
         </div>
     );
 };
