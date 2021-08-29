23f0682c80b3afbd843ad6ebc2a703f377dc0acb
Kory Koponen <koponenk@gmail.com>
2021-08-29 12:11:05 -0500

    moved date function to utils.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,31 +1,26 @@
 import "./comments.scss";
-import React from 'react';
-const options = { year: "numeric", month: "numeric", day: "numeric" };
+import React from "react";
+import { dateConvert } from "../../utils";
 
-
-const Comments = ({video}) => {
- 
-    return (
-        <section className="comments">
-        {video.comments.map((each) => (
-          <article key={each.id} className="comments__outerbox">
-            <div className="comments__img"></div>
-            <div className="comments__innerbox">
-              <div className="comments__name-box">
-                <div className="comments__name-box--name">{each.name}</div>
-                <div className="comments__name-box--date">
-                  {new Date(Number(each.timestamp)).toLocaleString(
-                    "en-US",
-                    options
-                  )}
-                </div>
+const Comments = ({ video }) => {
+  return (
+    <section className="comments">
+      {video.comments.map((each) => (
+        <article key={each.id} className="comments__outerbox">
+          <div className="comments__img"></div>
+          <div className="comments__innerbox">
+            <div className="comments__name-box">
+              <div className="comments__name-box--name">{each.name}</div>
+              <div className="comments__name-box--date">
+                {dateConvert(each.timestamp)}
               </div>
-              <div className="comments__text">{each.comment}</div>
             </div>
-          </article>
-        ))}
-      </section> 
-    );
+            <div className="comments__text">{each.comment}</div>
+          </div>
+        </article>
+      ))}
+    </section>
+  );
 };
 
 export default Comments;
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,15 @@
-import  axios from  'axios';
+import axios from "axios";
 export const url = "https://project-2-api.herokuapp.com/";
 export const api_key = "8c87e374-2f7e-4584-ae0c-ddd8fb0526e0";
 
+const options = { year: "numeric", month: "numeric", day: "numeric" };
 
+export function dateConvert (date) {new Date(Number(date)).toLocaleString(
+  "en-US",
+  options
+)}
 
 export const apiRequests = {
-    getAll: () => axios.get(`${url}videos?api_key=${api_key}`),
-    getDetails: id => axios.get(`${url}videos/${id}?api_key=${api_key}`),
-    postComment: (comment) => axios.post(`${url}`, comment),
-    deleteComment: id => axios.delete(`${url}/${id}`),
-    editComment: (id, comment) => axios.put(`${url}/${id}`, comment)
-}
+  getAll: () => axios.get(`${url}videos?api_key=${api_key}`),
+  getDetails: (id) => axios.get(`${url}videos/${id}?api_key=${api_key}`),
+};
