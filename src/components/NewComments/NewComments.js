3903386158d239520a4d8b0fe83415d3c152fe84
Kory Koponen <koponenk@gmail.com>
2021-08-29 14:30:16 -0500

    code prettify-ing

--- src/components/NewComments/NewComments.js
+++ src/components/NewComments/NewComments.js
@@ -1,43 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./newcomments.scss";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const NewComments = ({video, addComment}) => {
+const NewComments = ({ video, addComment }) => {
+  return (
+    <section className="new-comment">
+      <h3 className="new-comment__title">{video.comments.length} Comments</h3>
+      <div className="new-comment__outerbox">
+        <div className="new-comment__outerbox--img"></div>
 
-    return (
-        <section className="new-comment">
-        <h3 className="new-comment__title">{video.comments.length} Comments</h3>
-        <div className="new-comment__outerbox">
-          <div className="new-comment__outerbox--img"></div>
-
-          <form
-            onSubmit={addComment}
-            className="new-comment__outerbox--form"
-            action="submit"
-            id="commentForm"
-          >
-            <div className="new-comment__outerbox--inputbox">
-            <label /*for="comment"*/ className="new-comment__outerbox--form--label">
+        <form
+          onSubmit={addComment}
+          className="new-comment__outerbox--form"
+          action="submit"
+          id="commentForm"
+        >
+          <div className="new-comment__outerbox--inputbox">
+            <label
+              HTMLfor="comment"
+              className="new-comment__outerbox--form--label"
+            >
               Join the Conversation
             </label>
             <textarea
-            className="new-comment__outerbox--form--textarea"
+              className="new-comment__outerbox--form--textarea"
               id="comment"
-              // for="comment"
+              HTMLfor="comment"
               name="comment"
               type="reset"
               rows="5"
               cols="20"
               placeholder="Write comment here"
             ></textarea>
-            </div>
-            <Link to="/" className="new-comment__outerbox--form--link"> <button type="submit" className="new-comment__outerbox--form--button">
+          </div>
+          <Link to="/" className="new-comment__outerbox--form--link">
+            {" "}
+            <button
+              type="submit"
+              className="new-comment__outerbox--form--button"
+            >
               Comment
-            </button></Link>
-          </form>
-        </div>
-      </section>
-    );
+            </button>
+          </Link>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default NewComments;
